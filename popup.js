8967073b4eb0cb32b4d9dad16982cac1418182bc
minalsontakke <minalsontakke@users.noreply.github.com>
2015-01-02 11:25:59 +0000

    popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+	var txt = document.createTextNode("New Plugin Created");
+	var div = document.createElement("div");
+	div.appendChild(txt);
+	document.body.appendChild(div);
+}
